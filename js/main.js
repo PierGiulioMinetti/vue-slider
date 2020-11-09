9a6ccf766b9a3b09c053ee599fd304fb3b8beafa
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-11-09 16:44:40 +0100

    Gallery Working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
+
 const app = new Vue ({
     el : '#app',
-    data : {
 
+    data : {
+        indexPhoto : 0,
+        photos : [
+            './images/image1.jpg',
+            './images/image2.jpg',
+            './images/image3.jpg',
+            './images/image4.jpg'
+        ],
+
+    },
+    methods : {
+        nextPhoto(){
+            this.indexPhoto++;
+
+            if (this.indexPhoto > (this.photos.length - 1)){
+               this.indexPhoto = 0;
+            }
+        },
+        prevPhoto(){
+            this.indexPhoto--;
+            if (this.indexPhoto < 0){
+                this.indexPhoto = (this.photos.length - 1);
+             }
+        }
     }
 });
